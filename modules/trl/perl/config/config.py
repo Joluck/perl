b023f09c9aef56dc193a03b3a6362cb2ba863487
Joluck <997529190@qq.com>
2026-06-02 13:14:20 +0800

    fix

--- modules/trl/perl/config/config.py
+++ modules/trl/perl/config/config.py
@@ -28,6 +28,7 @@ class PeftConfig:
     target_modules: List[str] = field(
         default_factory=lambda: ["q_proj", "v_proj", "k_proj", "o_proj", "up_proj", "down_proj"]
     )
+    init_weights: str = True  # for lora/dora/rslora init, e.g. True, "pissa", "pissa_niter_4"
     merge_after_training: bool = False
 
 
