9f6d5cebafc70204b257ce68ceeca8bf4e099844
Joluck <997529190@qq.com>
2026-06-08 21:25:45 +0800

    1

--- modules/trl/perl/train.py
+++ modules/trl/perl/train.py
@@ -99,9 +99,9 @@ def train(
         logger.info(f"Lora configured successfully")
 
     # 4.Training configuration
-    training_kwargs = vars(args.training).copy()
-    training_kwargs.pop("max_prompt_length", None)
-    training_args = GRPOConfig(**training_kwargs)
+    training_args = GRPOConfig(
+        **vars(args.training),
+    )
 
     # 5.Train
     logger.info(f"Training model with GRPO")
